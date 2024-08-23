4799f4a0e5e7c826d52348d2edc617930a65d507
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-23 16:36:54 +0200

    comments lineup

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+// Recuperare elementi dal DOM
+// Reagire al click sul pulsante play che fa vedere la griglia
+// In ogni cella devono comparire i numeri in ordine da 1 a 100;
+// Reagire al click dell'utente che fa apparire il numero cliccato in console
+// Reagire al click dell'utente che fa cambiare il colore delle celle
+
+//Recupero elementi dal DOM
+const grid = document.getElementById('grid');
+const select = document.querySelector('select');
+const button = document.querySelector('button');
+
